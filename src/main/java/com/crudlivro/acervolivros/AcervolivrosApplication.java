1bce13debb6d423d35b83ac0ecbd4f6bf5a5e38e
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-08-18 22:59:39 -0300

    chore: setup inicial d projeto com Spring initializr

---
+++ src/main/java/com/crudlivro/acervolivros/AcervolivrosApplication.java
@@ -0,0 +1,13 @@
+package com.crudlivro.acervolivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervolivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervolivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudlivro/acervolivros/AcervolivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudlivro.acervolivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervolivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
