cc3ea6af4684ebdb15c9a01a559c65e4d7f84aff
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-08-20 21:12:06 -0300

    feat: adiciona endpoint GET /livros para listagem

--- src/main/java/com/crudlivro/acervolivros/controller/LivroController.java
+++ src/main/java/com/crudlivro/acervolivros/controller/LivroController.java
@@ -2,10 +2,14 @@ package com.crudlivro.acervolivros.controller;
 
 import com.crudlivro.acervolivros.model.Livro;
 import com.crudlivro.acervolivros.service.LivroService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/livros")
@@ -21,5 +25,9 @@ public class LivroController {
         return service.cadastrar(livro);
     }
 
+    @GetMapping
+    public List<Livro> listar(){
+        return service.listar();
+    }
 
 }
